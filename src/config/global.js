d6d2b39b4895ba098859f099ebb3f47d12aead27
Francisco Lizcano <flizcanor@icloud.com>
2022-07-29 18:38:38 -0500

    se actualiza descargable

--- src/config/global.js
+++ src/config/global.js
@@ -296,20 +296,17 @@ export default {
       {
         nombre: 'Caterine Bedoya Mejía',
         cargo: 'Diseñadora Instruccional',
-        centro:
-          'Centro de Gestión Industrial - Regional Distrito Capital',
+        centro: 'Centro de Gestión Industrial - Regional Distrito Capital',
       },
       {
         nombre: 'Andrés Felipe Velandia Espitia',
         cargo: 'Asesor Metodológico',
-        centro:
-          'Centro de Diseño y Metrología - Regional Distrito Capital',
+        centro: 'Centro de Diseño y Metrología - Regional Distrito Capital',
       },
       {
         nombre: 'Julia Isabel Roberto',
         cargo: 'Corrector de estilo',
-        centro:
-          'Centro de Diseño y Metrología - Regional Distrito Capital',
+        centro: 'Centro de Diseño y Metrología - Regional Distrito Capital',
       },
     ],
     desarrolloProducto: [
